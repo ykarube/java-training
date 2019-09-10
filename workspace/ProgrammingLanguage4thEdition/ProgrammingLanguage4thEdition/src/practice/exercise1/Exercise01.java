2d56d8cc64cebaf545de7766d18fe7ba13f78a6e
ykarube <ykaru.dev@gmail.com>
2019-09-10 16:26:58 +0900

    add:練習問題1.1

---
+++ workspace/ProgrammingLanguage4thEdition/ProgrammingLanguage4thEdition/src/practice/exercise1/Exercise01.java
@@ -0,0 +1,11 @@
+package practice.exercise1;
+
+/*
+ * 練習問題1.1
+ * HellowWorldを入力、コンパイル、実行する
+ */
+public class Exercise01 {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld.");
+	}
+}
