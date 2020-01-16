dc8d83222cec84b25b75c7923ad4f7e204eaeb43
ykarube <ykaru.dev@gmail.com>
2020-01-17 01:50:54 +0900

    edit

--- workspace/jpl/src/ch14/ex10/ThreadPool.java
+++ workspace/jpl/src/ch14/ex10/ThreadPool.java
@@ -4,7 +4,19 @@
  */
 package ch14.ex10;
 
-//TODO
+import java.util.LinkedList;
+
+/**
+	1.2.4 JPL 追加練習問題
+		第14 章の追加問題として練習問題10 を解いてもらいます。練習問題は、以下のリポジトリの
+		ThreadPool.java とThreadPoolTest.java の二つのソースファイルです。
+			https://github.com/YoshikiShibata/jpltest/tree/master/jpl/ch14/ex10
+		ファイル内のpackage 宣言をjpl.ch14.ex10 からch14.ex10 へ修正して、
+		各人の		jpl/src/ch14/ex10 とjpl/test/ch14/ex10 ディレクトリの下に、ThreadPool.java とThreadPoolTest.java を配置してださい。
+		練習問題の内容は以下の通りです。
+		• ThreadPoolTest.java は完成したテストコードであり修正しない
+		• ThreadPoolTest.java 内のテストがすべて合格するようにThreadPool.java を完成させる
+ */
 
 /**
  * Simple Thread Pool class.
@@ -25,20 +37,31 @@ package ch14.ex10;
  *
  *  @author Yoshiki Shibata
  */
-public class ThreadPool {
-	private final int size_;
+public class ThreadPool {
 
-	 /**
+    private ThreadPoolQueue<Runnable> queue;
+    private boolean isStarted_;
+
+    //workerThreadパタンを利用：
+	//http://www.hyuki.com/dp/dp2_ch08.pdf
+    private WorkerThread[] workerThreads;
+
+    /**
      * Constructs ThreadPool.
      *
      * @param queueSize the max size of queue
      * @param numberOfThreads the number of threads in this pool.
-     *
      * @throws IllegalArgumentException if either queueSize or numberOfThreads
-     *         is less than 1
+     *             is less than 1
      */
-    public ThreadPool(int queueSize, int numberOfThreads) {
-       throw new AssertionError("Not Implemented Yet");
+    public ThreadPool(final int queueSize, final int numberOfThreads) {
+    	if ( queueSize < 1 || numberOfThreads < 1 ) {
+    		throw new IllegalArgumentException();
+    	}
+        workerThreads = new WorkerThread[numberOfThreads];
+        queue = new ThreadPoolQueue<Runnable>(queueSize);
+        for (int i = 0; i < workerThreads.length; i++)
+            workerThreads[i] = new WorkerThread("thread-["+i+"]");
     }
 
     /**
@@ -47,31 +70,138 @@ public class ThreadPool {
      * @throws IllegalStateException if threads has been already started.
      */
     public void start() {
-        throw new AssertionError("Not Implemented Yet");
+        if (isStarted_)
+            throw new IllegalStateException("Already started.");
+        for (int i = 0; i < workerThreads.length; i++) {
+            try {
+                workerThreads[i].start();
+            } catch (IllegalThreadStateException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+        isStarted_ = true;
     }
 
     /**
-     * Stop all threads gracefully and wait for their terminations.
-	 * All requests dispatched before this method is invoked must complete
-	 * and this method also will wait for their completion.
+     * Stop all threads and wait for their terminations.
      *
      * @throws IllegalStateException if threads has not been started.
      */
     public void stop() {
-        throw new AssertionError("Not Implemented Yet");
+    	for (int i = 0; i < workerThreads.length; i++) {
+            if (workerThreads[i].isAlive()) {
+            	workerThreads[i].stopRequest();
+                try {
+                	System.out.println("[log] stop & join start:" + workerThreads[i].getName());
+                	workerThreads[i].join();
+                    System.out.println("[log] stop & join end:" + workerThreads[i].getName());
+                } catch (InterruptedException e) {
+                    System.err.println("err");
+                }
+            } else {
+                throw new IllegalStateException();
+            }
+        }
     }
 
     /**
-     * Executes the specified Runnable object, using a thread in the pool.
-     * run() method will be invoked in the thread. If the queue is full, then
-     * this method invocation will be blocked until the queue is not full.
+     * Executes the specified Runnable object, using a thread in the pool. run()
+     * method will be invoked in the thread. If the queue is full, then this
+     * method invocation will be blocked until the queue is not full.
      *
      * @param runnable Runnable object whose run() method will be invoked.
-     *
      * @throws NullPointerException if runnable is null.
      * @throws IllegalStateException if this pool has not been started yet.
      */
-    public void dispatch(Runnable runnable) {
-        throw new AssertionError("Not Implemented Yet");
+    public synchronized void dispatch(Runnable runnable) {
+        if (runnable == null) {
+            throw new NullPointerException();
+        }
+        if (!isStarted_) {
+            throw new IllegalStateException("Not statrted.");
+        }
+        queue.add(runnable);
+        System.out.println("[log] dispatch: queue size" + queue.size);
+    }
+
+    class WorkerThread extends Thread {
+
+        private boolean stopped;
+
+        public WorkerThread(String name) {
+			super(name);
+			stopped = false;
+		}
+
+        @Override
+        public void run() {
+            Runnable runnable = null;
+            while (!stopped) {
+                runnable = queue.poll();
+                if (runnable != null)
+                    runnable.run();
+            }
+        }
+
+        private void stopRequest() {
+            stopped = true;
+            queue.stop();
+        }
+    }
+
+    class ThreadPoolQueue<Runnable> {
+
+        private final int size;
+        private LinkedList<Runnable> list;
+
+        public ThreadPoolQueue(int size) {
+            if (size < 1) {
+                throw new IllegalArgumentException("Illegal size of queue.");
+            }
+            this.size = size;
+            list = new LinkedList<Runnable>();
+        }
+
+        /**
+         * Appends the specified element to the end of this list.
+         *
+         * @param e element to be appended to this list
+         * @return true, or false if interrupted on waiting for polling
+         */
+        public synchronized boolean add(Runnable runnable) {
+            while (list.size() >= size)
+                try {
+                    wait();
+                } catch (InterruptedException e1) {
+                    return false;
+                }
+            boolean result = list.add(runnable);
+            notifyAll();
+            System.out.println("[log] queue add: size" + list.size());
+            return result;
+        }
+
+        /**
+         * Retrieves and removes the head of this list.
+         *
+         * @return the head of this list, or null if interrupted on waiting for adding
+         */
+        public synchronized Runnable poll() {
+            if (list.isEmpty())
+                try {
+                    wait();
+                } catch (InterruptedException e1) {
+                    return null;
+                }
+            Runnable e = list.poll();
+            notifyAll();
+            System.out.println("[log] poll: list size" + list.size());
+            return e;
+        }
+
+        public synchronized void stop() {
+            notifyAll();
+            System.out.println("[log] stop;");
+        }
     }
 }
