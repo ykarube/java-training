2e747685cc852751420483c3d8d1fcf12ed1c51b
ykarube <ykaru.dev@gmail.com>
2020-10-14 00:26:51 +0900

    ch01 push

---
+++ workspace/js8ri/src/ch01/ex07/AndThen.java
@@ -0,0 +1,20 @@
+package ch01.ex07;
+
+public class AndThen {
+
+	public static void main(String[] args) {
+		andThen(()-> {
+		    System.out.println("first run.");
+		  }, ()->{
+		    System.out.println("second run.");
+		  }).run();
+	}
+
+    public static Runnable andThen(Runnable first, Runnable second) {
+        return () -> {
+            first.run();
+            second.run();
+        };
+    }
+
+}
